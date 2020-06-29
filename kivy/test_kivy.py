f7eb1cee191551f0e7209ace9ba63b7d2e9cc277
vincent-nsi <63296538+vincent-nsi@users.noreply.github.com>
2020-06-29 20:07:33 +0200

    texte interactif kivy

--- kivy/test_kivy.py
+++ kivy/test_kivy.py
@@ -1,9 +1,14 @@
 import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
 
 class MonInterface(Widget):
-    pass
+    entre = ObjectProperty(None)
+    message = ObjectProperty(None)
+    def clicBouton(self):
+        self.message.text = f"J'ai bien lu : {self.entre.text}"
+
 
 class MonAppli(App):
     def build(self):
